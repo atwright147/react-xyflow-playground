0c9fae03e8a441fff6846fea2be075a0e0a8aca7
Andy Wright <atwright147@gmail.com>
2025-01-30 17:10:46 +0000

    tidy up

--- src/stores/Flow.store.ts
+++ src/stores/Flow.store.ts
@@ -21,111 +21,9 @@ import { devtools } from 'zustand/middleware';
 import type { CustomNodeData } from '../../types/nodes';
 
 //#region Nodes Config
-const initialNodes: Node<CustomNodeData>[] = [
-  // {
-  //   id: 'text-1',
-  //   type: 'textUpdater',
-  //   position: { x: 50, y: 50 },
-  //   data: {
-  //     label: 'Value',
-  //     headerBackground: 'purple',
-  //     headerForeground: 'white',
-  //     value: 5,
-  //     valueType: 'number',
-  //   },
-  // },
-  // {
-  //   id: 'text-2',
-  //   type: 'textUpdater',
-  //   position: { x: 50, y: 200 },
-  //   data: {
-  //     label: 'Value 2',
-  //     headerBackground: 'purple',
-  //     headerForeground: 'white',
-  //     value: 10,
-  //     valueType: 'number',
-  //   },
-  // },
-  // {
-  //   id: 'maths-1',
-  //   type: 'maths',
-  //   position: { x: 400, y: 100 },
-  //   data: {
-  //     label: 'Maths',
-  //     headerBackground: 'blue',
-  //     headerForeground: 'white',
-  //     value: 0,
-  //     valueType: 'number',
-  //     operation: 'add',
-  //   },
-  // },
-  // {
-  //   id: 'times-two-1',
-  //   type: 'timesTwo',
-  //   position: { x: 400, y: 300 },
-  //   data: {
-  //     label: 'Times Two',
-  //     headerBackground: 'blue',
-  //     headerForeground: 'white',
-  //     value: 0,
-  //     valueType: 'number',
-  //     operation: 'add',
-  //   },
-  // },
-  // {
-  //   id: 'log-1',
-  //   type: 'log',
-  //   position: { x: 700, y: 100 },
-  //   data: {
-  //     label: 'Log',
-  //     headerBackground: 'blue',
-  //     headerForeground: 'white',
-  //     value: 0,
-  //     valueType: 'number',
-  //   },
-  // },
-  // {
-  //   id: 'log-2',
-  //   type: 'log',
-  //   position: { x: 700, y: 250 },
-  //   data: {
-  //     label: 'Log',
-  //     headerBackground: 'blue',
-  //     headerForeground: 'white',
-  //     value: 0,
-  //     valueType: 'number',
-  //   },
-  // },
-  // {
-  //   id: 'log-3',
-  //   type: 'log',
-  //   position: { x: 700, y: 400 },
-  //   data: {
-  //     label: 'Log',
-  //     headerBackground: 'blue',
-  //     headerForeground: 'white',
-  //     value: 0,
-  //     valueType: 'number',
-  //   },
-  // },
-];
+const initialNodes: Node<CustomNodeData>[] = [];
 
-const initialEdges: Edge[] = [
-  // {
-  //   source: 'text-1',
-  //   sourceHandle: 'text-1',
-  //   target: 'maths-1',
-  //   targetHandle: 'a',
-  //   id: 'xy-edge__text-1text-1-maths-1a',
-  // },
-  // {
-  //   source: 'text-2',
-  //   sourceHandle: 'text-2',
-  //   target: 'maths-1',
-  //   targetHandle: 'b',
-  //   id: 'xy-edge__text-2text-2-maths-1b',
-  // },
-];
+const initialEdges: Edge[] = [];
 //#endregion
 
 export interface FlowStore {
@@ -182,10 +80,10 @@ const useFlowStore = create<FlowStore>()(
         set({ selectedNodeType: node.type });
       },
       draggedItem: null,
-      handleDragStart: (event) => {
+      handleDragStart: (event: React.DragEvent) => {
         // set({ draggedItem: event.active.id });
       },
-      handleDragEnd: (event) => {
+      handleDragEnd: (event: React.DragEvent) => {
         set({ draggedItem: null });
 
         const nodes = get().nodes;
